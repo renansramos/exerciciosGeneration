3dd536adbb980f5651e7d3519f5b249e3d711ee3
Renan Ramos <86578128+renansramos@users.noreply.github.com>
2024-04-20 00:29:44 -0300

    Create Atividade4.java

---
+++ Atividade4.java
@@ -0,0 +1,36 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class atividade4 {
+
+	public static void main(String[] args) {
+		
+	/*Leia quatro valores float (n1, n2, n3, n4). 
+	A seguir, calcule e mostre a diferença do 
+	produto entre o n1 e n2 pelo produto entre 
+	o n3 e o n4. Veja os exemplos abaixo:*/
+		
+	Scanner leia = new Scanner(System.in);
+	
+	float numero1;
+	float numero2;
+	float numero3;
+	float numero4;
+	
+	System.out.println("Insira o primeiro número: ");
+	numero1 = leia.nextFloat();
+	
+	System.out.println("Insira o segundo número: ");
+	numero2 = leia.nextFloat();
+	
+	System.out.println("Insira o terceiro número: ");
+	numero3 = leia.nextFloat();
+	
+	System.out.println("Insira o quarto número: ");
+	numero4 = leia.nextFloat();
+		
+	System.out.println("Diferença: " + ((numero1 * numero2 ) - (numero3 * numero4)));
+		
+	}
+}
